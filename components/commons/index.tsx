7bef497c8fb08513bc295a1465766d3915e4f63f
teknixproject <project@teknixcorp.com>
2025-04-18 10:47:17 +0700

    Update index - Add TextGradientIni

--- components/commons/index.tsx
+++ components/commons/index.tsx
@@ -19,4 +19,5 @@ export const components = {
   dropdown: dynamic(() => import('./Dropdown')),
   collapse: dynamic(() => import('./Collapse')),
   bahu_bali_about: dynamic(() => import("./BahuBaliAbout")),
-  text_gradient: dynamic(() => import("./TextGradient")),};
+  text_gradient: dynamic(() => import("./TextGradient")),  text_gradient_ini: dynamic(() => import("./TextGradientIni")),
+};
